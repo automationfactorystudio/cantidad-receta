8e56c16aa01780e72c1b14a825e1841bb540c623
AutomationFactoryStudio <olgaamatriain@gmail.com>
2026-09-10 14:08:24 +0200

    Add files via upload

---
+++ sw-calculadora-recetas.js
@@ -0,0 +1,19 @@
+const PREFIX = 'calculadora-recetas-' + new URL(self.registration.scope).pathname + '-';
+const CACHE = PREFIX + 'v3';
+const ASSETS = ['./', './index.html', './manual.html', './manifest-calculadora-recetas.json', './icon-appdeboy-192.png', './icon-appdeboy-512.png'];
+self.addEventListener('install', event => {
+ event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+self.addEventListener('activate', event => {
+ event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key.startsWith(PREFIX) && key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim()));
+});
+self.addEventListener('fetch', event => {
+ const url = new URL(event.request.url);
+ if (event.request.method !== 'GET' || url.origin !== self.location.origin || !url.href.startsWith(self.registration.scope)) return;
+ event.respondWith(caches.open(CACHE).then(async cache => {
+  const stored = await cache.match(event.request, {ignoreSearch: true});
+  if (stored) return stored;
+  if (event.request.mode === 'navigate') return (await cache.match('./index.html')) || fetch(event.request);
+  return fetch(event.request);
+ }));
+});
